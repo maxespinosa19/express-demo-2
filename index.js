6f011baa1c3044470f36f74c2f099ac9c9e4c330
Max Espinosa <maxespinosa@Maxs-MacBook-Air.local>
2023-06-20 16:56:32 -0400

    WORKING

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express"
+import cors from "cors"
+import { getFlowers, addNewFlower } from "./src/flowers.js"
+const PORT = 3000
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get("/flowers", getFlowers)
+app.post('/flowers', addNewFlower)
+ 
+app.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}...`)
+})
+
---
+++ src/flowers.js
@@ -0,0 +1,16 @@
+const flowers = [
+    {type: 'marigold', color: 'orange'} ,
+    {type: 'daisy', color: 'yellow'} ,
+    {type: 'tulip', color: 'red'},
+
+]
+
+export function getFlowers(req,res) {
+    res.send(flowers)
+}
+
+export function addNewFlower(req,res){
+    const newFlower = req.body
+    flowers.push(newFlower)
+    res.status(201).send({success: true, message: 'Flower added.'})
+}
